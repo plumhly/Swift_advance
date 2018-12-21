d33969be3f8b62ac2e6298034ce7c06710fad992
plum <>
2018-12-21 12:41:15 +0800

    学习slice

--- Swift_advance/SwiftAdvance.playground/Contents.swift
+++ Swift_advance/SwiftAdvance.playground/Contents.swift
@@ -400,66 +400,101 @@ do {
 //let a = ""
 //let i = a.startIndex
 //a[i]
+//
+//let good = "good"
+//let index = good.endIndex
+//good[index..<index]
+//
+//extension Substring {
+//    var nextWordRange: Range<Index> {
+//        let start = drop(while: {$0 == " "})
+//        let end = start.firstIndex(where: {$0 == " "}) ?? endIndex
+//        return start.startIndex..<end
+//    }
+//}
+//
+//struct Words: Collection {
+//    let string: Substring
+//    let startIndex: WordsIndex
+//
+//    init(_ s: String) {
+//        self.init(s[...])
+//    }
+//
+//    private init(_ s: Substring) {
+//        self.string = s
+//        self.startIndex = WordsIndex(string.nextWordRange)
+//    }
+//
+//    var endIndex: WordsIndex {
+//        let e = string.endIndex
+//        return WordsIndex(e..<e)
+//    }
+//}
+//
+//extension Words {
+//    subscript(index: WordsIndex) -> Substring {
+//        return string[index.range]
+//    }
+//}
+//
+//extension Words {
+//    func index(after i: WordsIndex) -> WordsIndex {
+//        guard i.range.upperBound < string.endIndex else {return endIndex}
+//        let remainder = string[i.range.upperBound...]
+//        return WordsIndex(remainder.nextWordRange)
+//
+//    }
+//}
+//
+//extension Words {
+//    subscript(range: Range<WordsIndex>) -> Words {
+//      let start = range.lowerBound.range.lowerBound
+//      let end = range.upperBound.range.upperBound
+//        return Words(string[start..<end])
+//    }
+//}
+//
+//struct WordsIndex: Comparable {
+//    fileprivate let range: Range<Substring.Index>
+//    fileprivate init(_ value: Range<Substring.Index>) {
+//      self.range = value
+//    }
+//
+//    static func < (lhs: Words.Index, rhs: Words.Index) -> Bool {
+//        return lhs.range.lowerBound < rhs.range.lowerBound
+//    }
+//
+//    static func == (lhs: Words.Index, rhs: Words.Index) -> Bool {
+//        return lhs.range.lowerBound == rhs.range.lowerBound
+//    }
+//}
+//
+//let words = Words("hello world text")
 
-let good = "good"
-let index = good.endIndex
-good[index..<index]
+//let cities = ["1", "2", "3", "4", "5", "6"]
+//let slice = cities[2...4]
+//slice.startIndex
+//cities.indices
 
-extension Substring {
-    var nextWordRange: Range<Index> {
-        let start = drop(while: {$0 == " "})
-        let end = start.firstIndex(where: {$0 == " "}) ?? endIndex
-        return start.startIndex..<end
+struct PrefixIterator<Base: Collection>: IteratorProtocol, Sequence {
+    let base: Base
+    var offset: Base.Index
+    
+    init(_ base: Base) {
+        self.base = base
+        self.offset = base.startIndex
+    }
+    
+    mutating func next() -> Base.SubSequence? {
+        guard offset != base.endIndex else {
+            return nil
+        }
+        base.formIndex(after: &offset)
+        return base.prefix(upTo: offset)
     }
 }
 
-struct Words: Collection {
-    let string: Substring
-    let startIndex: WordsIndex
-    
-    init(_ s: String) {
-        self.init(s[...])
-    }
-    
-    private init(_ s: Substring) {
-        self.string = s
-        self.startIndex = WordsIndex(string.nextWordRange)
-    }
-    
-    var endIndex: WordsIndex {
-        let e = string.endIndex
-        return WordsIndex(e..<e)
-    }
-}
+let number = [1, 2, 3]
+Array(PrefixIterator(number))
 
-extension Words {
-    subscript(index: WordsIndex) -> Substring {
-        return string[index.range]
-    }
-}
-
-extension Words {
-    func index(after i: WordsIndex) -> WordsIndex {
-        guard i.range.upperBound < string.endIndex else {return endIndex}
-        let remainder = string[i.range.upperBound...]
-        return WordsIndex(remainder.nextWordRange)
-        
-    }
-}
-
-struct WordsIndex: Comparable {
-    fileprivate let range: Range<Substring.Index>
-    fileprivate init(_ value: Range<Substring.Index>) {
-      self.range = value
-    }
-    
-    static func < (lhs: Words.Index, rhs: Words.Index) -> Bool {
-        return lhs.range.lowerBound < rhs.range.lowerBound
-    }
-    
-    static func == (lhs: Words.Index, rhs: Words.Index) -> Bool {
-        return lhs.range.lowerBound == rhs.range.lowerBound
-    }
-}
-
-Array(Words("hello world text")).prefix(2)
